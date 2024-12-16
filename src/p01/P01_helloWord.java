45c78d59c53927948710332cae0236b12da629d7
nagihanmag <nagihan.kartal@outlook.com>
2024-12-16 12:58:19 +0300

    secont commit

--- src/p01/P01_helloWord.java
+++ src/p01/P01_helloWord.java
@@ -3,5 +3,6 @@ package p01;
 public class P01_helloWord {
     public static void main(String[] args) {
         System.out.println("hello word");
+        System.out.println("Hello GitHub");
     }
 }
--- src/p02/P01.java
+++ src/p02/P01.java
@@ -1,9 +1,18 @@
 package p02;
 
+import java.util.Scanner;
+
 public class P01 {
     public static void main(String[] args) {
         int a = 128 ;
         int b = 17 ;
         System.out.println("c="+(a+b));
+        Scanner scan = new Scanner(System.in);
+
+        int sayi = scan.nextInt();
+
+        if (sayi%2==0){
+            System.out.println("GIRDIGINIZ SAYI CIFT SAYIDIR");
+        }else System.out.println("Girdiginiz sayi cift sayi degildir ");
     }
 }
